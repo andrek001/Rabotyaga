681f1cb468a23d88ae4109548298b1f33ce8ee0a
andrek001 <andrewfml@gmail.com>
2023-08-08 23:42:09 +0300

    Init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener error = (i) -> i == 33;
+        Worker worker = new Worker(listener, error);
+        worker.start();
+    }
+}
